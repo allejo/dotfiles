0e397dd22d79c1513394ce34d9df619d92729561
Vladimir Jimenez <allejo@me.com>
2022-08-23 17:56:07 -0700

    Add new `pr-files` alias

--- profile.ps1
+++ profile.ps1
@@ -1,4 +1,7 @@
-Set-Alias -Name open -Value explorer
+Set-Alias -Name open -Value Start
+
+Function PrFiles { gh pr-files | ForEach-Object -Process {[void](Test-Path $_) && Rider.cmd $_ && Start-Sleep -Milliseconds 50} }
+Set-Alias -Name pr-files -Value PrFiles
 
 Function EditAliases { notepad $((Split-Path $profile -Parent) + "\profile.ps1") }
 Set-Alias -Name edit-aliases -Value EditAliases
